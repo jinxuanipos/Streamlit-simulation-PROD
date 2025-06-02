c58ebc05623da629c9984d916fea30af269953e9
jinxuanipos <63656078+jinxuanipos@users.noreply.github.com>
2025-06-02 15:59:29 +0800

    Update OutE_S_1.py

--- OutE_S_1.py
+++ OutE_S_1.py
@@ -10,23 +10,20 @@ import io
 # === STREAMLIT APP ===
 st.title("Running FOA Simulations")
 
-# --- User Input ---
-# Create 2 rows, each with 2 columns
-row1 = st.container()
-row2 = st.container()
-
 with row1:
     col1, col2 = st.columns(2)
+
     with col1:
         st.subheader("Demand + S&E growth")
-	pphgrowth = st.slider("Enter Growth Rate of PPH Usage Rate Y-o-Y", min_value=0, max_value=20, value=10)
+        pphgrowth = st.slider("Enter Growth Rate of PPH Usage Rate Y-o-Y", min_value=0, max_value=20, value=10)
         eot = st.selectbox("Select EOT Waiver Success Rate", ["26%", "30%", "35%"])
+
     with col2:
         st.subheader("Capacity")
         hire = st.selectbox("Select Hiring Plan", 
                     ["Accelerated - Hire additional 20 by Jan 26",
                      "Moderate - Hire additional 10 by Jan 26",
-		     "Paced - Hire additional 20 by Jul 26"])
+                     "Paced - Hire additional 20 by Jul 26"])
         AIgainschoice = st.selectbox("Select projected S&E Productivity gains from PAS and Report Drafter", 
                     ["Best - 55% by Jan30",
                      "Better - 45% by Jan30",
